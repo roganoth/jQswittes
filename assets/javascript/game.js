5d1833bd9e25f05f91acd85409f2dc2d57ba7d7c
dan <chronos937@gmail.com>
2019-10-01 18:31:14 -0400

    fixed js src link started working with array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
 var wordBank = ["PARADIGM","ALLOY","MINE","CRAFT","EVOLUTION","CLEAVER"]
 
+
 function newWord() {
-    Math.floor(Math.random(wordBank) + 1);
+    var tryAgain = Math.floor(Math.random()*wordBank.length);
+    console.log(tryAgain);
+    div.getElementById(guesses).innerHTML = wordBank[tryAgain];
+     
 }
 
