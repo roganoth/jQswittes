dfaf5e10680e647b85f94e5f2929dd180d2e102a
dan <chronos937@gmail.com>
2019-10-01 21:35:41 -0400

    added keyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,12 @@ var wordBank = ["PARADIGM","ALLOY","MINE","CRAFT","EVOLUTION","CLEAVER"]
 function newWord() {
     var tryAgain = Math.floor(Math.random()*wordBank.length);
     console.log(tryAgain);
-    div.getElementById(guesses).innerHTML = wordBank[tryAgain];
+    console.log(wordBank[tryAgain]);
      
+}
+
+document.onkeyup = function(event){
+    var userGuess = event.key;
+    console.log(userGuess);
 }
 
