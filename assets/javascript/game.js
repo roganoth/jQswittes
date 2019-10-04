51752b38789d22be298f0db755fad5ff6770fb75
dan demeo <chronos937@gmail.com>
2019-10-04 13:01:05 -0400

    moved textContent into correct scope but still getting a null error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,30 @@
-var wordBank = ["PARADIGM","ALLOY","MINE","CRAFT","EVOLUTION","CLEAVER"]
+var wrongGuessText = document.getElementById("#guessbox");
+var blankLetters = document.getElementById("#guessbox");
 
+wordBank = ["PARADIGM","ALLOY","MINE","CRAFT","EVOLUTION","CLEAVER"]
+var tryAgain = Math.floor(Math.random()*wordBank.length);
+console.log(wordBank[tryAgain]);
+var wrongGuess = [];
 
 function newWord() {
     var tryAgain = Math.floor(Math.random()*wordBank.length);
-    console.log(tryAgain);
     console.log(wordBank[tryAgain]);
+    blankLetters.textContent = "_ " * tryAgain.length;
      
 }
 
 document.onkeyup = function(event){
     var userGuess = event.key;
+    var upperUserGuess = userGuess.toUpperCase();
     console.log(userGuess);
-    document.getElementById("#guessbox").innerHTML("userGuess");
 
+    if(wordBank[tryAgain].includes(upperUserGuess)) {
+        console.log(upperUserGuess);
+    }
+
+    else {
+        wrongGuess.push(upperUserGuess);
+        wrongGuessText.textContent = "You've tried these letter: " + wrongGuess;
+
+    }
 }
-
